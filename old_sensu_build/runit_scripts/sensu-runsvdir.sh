37c36287f7566d2c382aff0353f94fa214bea0d4
Pete Cheslock <petecheslock@gmail.com>
2014-02-06 00:26:00 -0500

    Remove the temp old sensu build stuff to cleanup
    This is no longer needed - can leverage master on the origin

--- old_sensu_build/runit_scripts/sensu-runsvdir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# ensure the original path is exported for service scripts to use when choosing a ruby
-export ORIGINAL_PATH=$PATH
-export PATH=/opt/sensu/embedded/bin:$PATH
-exec /opt/sensu/embedded/bin/runsvdir -P /opt/sensu/service 'log: .................................................................................................................................'
