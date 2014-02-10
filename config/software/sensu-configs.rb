d81349e5b376e4eba86183dad4ab78d5bfd2c46e
Pete Cheslock <petecheslock@gmail.com>
2014-02-10 11:10:01 -0500

    Fix typos inthe sensu-configs package build

--- config/software/sensu-configs.rb
+++ config/software/sensu-configs.rb
@@ -1,25 +1,20 @@
 name "sensu-configs"
 
-sensu_config = "#{Omnibus.project_root}/sensu_configs"
+sensu_configs = "#{Omnibus.project_root}/sensu_configs"
 
 build do
-  command "rm -rf /etc/sensu"
-  command "rm -rf /etc/default/sensu"
-  command "rm -rf /etc/init.d/sensu-*"
-  command "rm -rf /usr/share/sensu"
-  command "rm -rf /var/log/sensu"
-  command "mkdir /etc/sensu"
-  command "cp -rf #{sensu_config}/sensu /etc/sensu"
-  command "cp -rf #{sensu_config}/default/* /etc/default/"
-  command "cp -rf #{sensu_config}/logrotate.d/* /etc/logrotate.d/"
-  command "cp -rf #{sensu_config}/init.d/* /etc/init.d/"
-  command "mkdir /usr/share/sensu"
-  command "cp -rf ./sensu_configs/init.d /usr/share/sensu/"
-  command "cp -rf ./sensu_configs/upstart /usr/share/sensu/"
-  command "cp -rf ./sensu_configs/systemd /usr/share/sensu/"
-  command "mkdir /var/log/sensu"
+  command "sudo mkdir /etc/sensu"
+  command "sudo cp -rf #{sensu_configs}/sensu /etc/sensu"
+  command "sudo cp -rf #{sensu_configs}/default/* /etc/default/"
+  command "sudo cp -rf #{sensu_configs}/logrotate.d/* /etc/logrotate.d/"
+  command "sudo cp -rf #{sensu_configs}/init.d/* /etc/init.d/"
+  command "sudo mkdir /usr/share/sensu"
+  command "sudo cp -rf #{sensu_configs}/init.d /usr/share/sensu/"
+  command "sudo cp -rf #{sensu_configs}/upstart /usr/share/sensu/"
+  command "sudo cp -rf #{sensu_configs}/systemd /usr/share/sensu/"
+  command "sudo mkdir /var/log/sensu"
 
   %w[plugins mutators handlers extensions].each do |dir|
-    command "mkdir /etc/sensu/#{dir}"
+    command "sudo mkdir /etc/sensu/#{dir}"
   end
 end
